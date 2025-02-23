1e0de7711c6d87850e72e55119bf89c511206c76
Mukto Akash <muktoakash@gmail.com>
2025-02-23 04:44:49 -0500

    Modify shell script to fil project

--- FreshWaterQuality/Processing/STEP2_script_C2RCC.sh
+++ FreshWaterQuality/Processing/STEP2_script_C2RCC.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-output_folder=/shared/Training/HYDR02_FreshWaterQuality_Trasimeno/Processing/C2RCC/
-metData=/shared/Training/HYDR02_FreshWaterQuality_Trasimeno/AuxData/Met_aux.csv
-paramFile=/shared/Training/HYDR02_FreshWaterQuality_Trasimeno/Processing/c2rcc_param.xml
+output_folder=./script_out
+metData=../HYDR02_FreshWaterQuality_Trasimeno/HYDR02_FreshWaterQuality_Trasimeno/AuxData/Met_aux.csv
+paramFile=./c2rcc_param.xml
 oldEnd=.dim
 newEnd=_C2RCC.dim
 
-for i in $(ls -d -1 /shared/Training/HYDR02_FreshWaterQuality_Trasimeno/Processing/Pre-processed/*.dim)
+for i in $(ls -d -1 ./Output/*.dim)
 do
     name=$(basename $i)
     idate=$(basename $i | cut -d '_' -f 4 | cut -d 'T' -f 1)
