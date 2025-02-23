0f97bc1380d370d5d1a6ad77998a090e5bf1bcc6
Mukto Akash <muktoakash@gmail.com>
2025-02-23 06:56:25 -0500

    Modify Step 3 script for current project

--- FreshWaterQuality/Processing/STEP3_script_BMATH.sh
+++ FreshWaterQuality/Processing/STEP3_script_BMATH.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-output_folder1=/shared/Training/HYDR02_FreshWaterQuality_Trasimeno/Processing/R705_R665/
-output_folder2=/shared/Training/HYDR02_FreshWaterQuality_Trasimeno/Processing/MCI/
+output_folder1=./step3_script_out/R705_R665/
+output_folder2=./step3_script_out/MCI/
 oldEnd=_C2RCC.dim
 newEnd1=_R705_R665.tif
 newEnd2=_MCI.tif
 
-for i in $(ls -d -1 /shared/Training/HYDR02_FreshWaterQuality_Trasimeno/Processing/C2RCC/*.dim)
+for i in $(ls -d -1 ./script_out/*.dim)
 do
     name=$(basename $i)
     output1=$output_folder1${name%$oldEnd}$newEnd1
     output2=$output_folder2${name%$oldEnd}$newEnd2
-    gpt /shared/Training/HYDR02_FreshWaterQuality_Trasimeno/Processing/STEP3_Graph_BMath.xml -PInput=$i -POutput1=$output1 -POutput2=$output2
+    gpt ./secondGraph.xml -PInput=$i -POutput1=$output1 -POutput2=$output2
 done
